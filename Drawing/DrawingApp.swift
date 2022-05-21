1f87800991a39f3b656fbe747841e3861254a796
Jae Cho <jjaecho@gmail.com>
2022-05-20 23:53:47 -0400

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Jae Cho on 5/20/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
