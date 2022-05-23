9756209d714957eb5d7cda50164162af90c6939c
Jae Cho <jjaecho@gmail.com>
2022-05-23 10:42:55 -0400

    43-1 Creating custom paths

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,20 +7,26 @@
 
 import SwiftUI
 
+struct Triangle: Shape {
+	func path(in rect: CGRect) -> Path {
+		var path = Path()
+		path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+		path.addLine(to: CGPoint(x:rect.minX, y: rect.maxY))
+		path.addLine(to: CGPoint(x:rect.maxX, y: rect.maxY))
+		path.addLine(to: CGPoint(x:rect.midX, y: rect.minY))
+		return path
+	}
+}
+
 struct ContentView: View {
     var body: some View {
-		 Path{ path in
-			 path.move(to: CGPoint(x: 200, y: 100))
-			 path.addLine(to: CGPoint(x:100, y: 300))
-			 path.addLine(to: CGPoint(x:300, y: 300))
-			 path.addLine(to: CGPoint(x: 200,y: 100))
-			 //path.closeSubpath()
-		 }
-		 //.fill(.blue)
-		 //.stroke(.blue, lineWidth: 10)
+		 Triangle()
+			// .fill(.red)
+			// .frame(width:300, height: 300)
 		 .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
