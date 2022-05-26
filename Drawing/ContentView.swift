1c32e1a975e02935be2595fd7354690f15cd226e
Jae Cho <jjaecho@gmail.com>
2022-05-25 21:57:07 -0400

    Day 44 ImagePaint

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -35,16 +35,14 @@ struct ContentView: View {
 	@State private var petalWidth = 100.0
     var body: some View {
 		 VStack{
-			 Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-				 //.stroke(.red, lineWidth: 1)
-				 .fill(.red, style: FillStyle(eoFill: true))
-			 Text("Offset")
-			 Slider(value: $petalOffset, in: -40...40)
-				 .padding([.horizontal, .bottom])
+			 Text("Hello World")
+				 .frame(width:300, height: 300)
+				 .border(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.1), width: 30)
 			 
-			 Text("Width")
-			 Slider(value: $petalWidth, in: 0...100)
-				 .padding(.horizontal)
+			 Capsule()
+				 .strokeBorder(ImagePaint(image: Image("Example"),scale:0.1),lineWidth: 20)
+				 .frame(width: 300, height: 300)
+
 		 }
 	 }
 }
