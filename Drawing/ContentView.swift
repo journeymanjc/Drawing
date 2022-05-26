fbf921d69e2f4319a47c19e6dd2db64690a320c9
Jae Cho <jjaecho@gmail.com>
2022-05-25 22:45:36 -0400

    Day 45 using saturation and blur

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -53,20 +53,12 @@ struct ContentView: View {
     var body: some View {
 		 VStack{
 			 ZStack{
-				 Circle()
-					 .fill(.red)
-					 .frame(width: 200 * amount)
-					 .offset(x: -50, y: -80)
-					 .blendMode(.screen)
-				 Circle()
-					 .fill(.green)
-					 .frame(width: 200 * amount)
-					 .offset(x: 50, y: -80)
-					 .blendMode(.screen)
-				 Circle()
-					 .fill(.blue)
-					 .frame(width: 200 * amount)
-					 .blendMode(.screen)
+				Image("Jae")
+					 .resizable()
+					 .scaledToFit()
+					 .frame(width: 200, height: 200)
+					 .saturation(amount)
+					 .blur(radius: (1 - amount) * 20)
 			 }
 			 .frame(width: 300, height: 300)
 			 
