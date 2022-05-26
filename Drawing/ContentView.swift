d3e3e1bbb62018ae61eb236a18d8887e3ad7beb0
Jae Cho <jjaecho@gmail.com>
2022-05-25 21:49:05 -0400

    Day 44 Using CGAffineTransform

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -6,43 +6,47 @@
 //
 
 import SwiftUI
-
-struct Triangle: Shape {
-	func path(in rect: CGRect) -> Path {
-		var path = Path()
-		path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-		path.addLine(to: CGPoint(x:rect.minX, y: rect.maxY))
-		path.addLine(to: CGPoint(x:rect.maxX, y: rect.maxY))
-		path.addLine(to: CGPoint(x:rect.midX, y: rect.minY))
-		return path
-	}
-}
-
-struct Arc : InsettableShape {
-	let startAngle : Angle
-	let endAngle : Angle
-	let clockwise: Bool
-	var insetAmount = 0.0
-	func path(in rect: CGRect) -> Path {
-		var path = Path()
-		path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: startAngle, endAngle: endAngle, clockwise: clockwise)
-		return path
-	}
+/**
+ Create a new empty path
+ Count from 0 up to pi * 2, one eight of pi each time which results in 16 petals.
+ rotation transform equal to the current number??? what does this mean?
+ */
+struct Flower: Shape {
+	//How much to move this petal away from the center
+	var petalOffset: Double = -20
+	//How wide to make each petal
+	var petalWidth: Double = 100
 	
-	func inset(by amount: CGFloat) -> some InsettableShape{
-		var arc = self
-		arc.insetAmount += amount
-		return arc
+	func path(in rect: CGRect) -> Path {
+		// The path that will hold all petals
+		var path = Path()
+		for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8){
+			let rotation = CGAffineTransform(rotationAngle: number)
+			let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+			let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
+			let rotatedPetal = originalPetal.applying(position)
+			path.addPath(rotatedPetal)
+		}
+		return path
 	}
 }
-
-
 struct ContentView: View {
+	@State private var petalOffset = -20.0
+	@State private var petalWidth = 100.0
     var body: some View {
-		 Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
-			 .strokeBorder()
-			 .frame(width: 300, height: 300)
-    }
+		 VStack{
+			 Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+				 //.stroke(.red, lineWidth: 1)
+				 .fill(.red, style: FillStyle(eoFill: true))
+			 Text("Offset")
+			 Slider(value: $petalOffset, in: -40...40)
+				 .padding([.horizontal, .bottom])
+			 
+			 Text("Width")
+			 Slider(value: $petalWidth, in: 0...100)
+				 .padding(.horizontal)
+		 }
+	 }
 }
 
 
